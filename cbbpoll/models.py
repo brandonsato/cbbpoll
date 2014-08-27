514432a45fad15df8049ebf07e951b30fb27e218
Brandon Sato <brandonsato@gmail.com>
2014-08-27 03:13:26 -0400

    add user model

---
+++ cbbpoll/models.py
@@ -0,0 +1,18 @@
+from cbbpoll import db
+
+ROLE_USER = 0
+ROLE_POLLSTER = 1
+ROLE_ADMIN = 5
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(20), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+    role = db.Column(db.SmallInteger, default = ROLE_USER)
+    accessToken = db.Column(db.String(30))
+    refreshToken = db.Column(db.String(30))
+    refreshAfter = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
