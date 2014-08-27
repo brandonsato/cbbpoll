f2eefb7b549a600fd66d4b307255661259d744b7
Brandon Sato <brandonsato@gmail.com>
2014-08-27 19:30:30 -0400

    basic reddit auth for login

--- cbbpoll/__init__.py
+++ cbbpoll/__init__.py
@@ -12,8 +12,6 @@ lm.init_app(app)
 lm.login_view = 'login'
 
 r = praw.Reddit('/r/CollegeBasketball User Poll test')
-r.set_oauth_app_info(app.config['REDDIT_CLIENT_ID'], 
-			app.config['REDDIT_CLIENT_SECRET'], 
-			app.config['REDDIT_REDIRECT_URI)'])
+r.set_oauth_app_info(app.config['REDDIT_CLIENT_ID'], app.config['REDDIT_CLIENT_SECRET'], app.config['REDDIT_REDIRECT_URI'])
 
 from cbbpoll import views, models
