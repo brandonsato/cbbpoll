c5a33cc89894361814a8684372aa18f524f061d4
Brandon Sato <brandonsato@gmail.com>
2014-08-27 16:54:18 -0400

    begin implementing authentication

--- cbbpoll/forms.py
+++ cbbpoll/forms.py
@@ -3,5 +3,4 @@ from wtforms import TextField, BooleanField
 from wtforms.validators import Required
 
 class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
